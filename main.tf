2d8e650e95631673965f79a22174faac481be32c
Asadkhanrtx <asadchamp109@gmail.com>
2026-05-21 17:32:36 +0530

    remote module

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_instance" "demo" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags = {
+        Name = var.instance_name
+    }
+}
